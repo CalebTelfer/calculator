1d37f97f64142339e670084ea7bdc54ab9142e9c
caleb <calebtelfe@gmail.com>
2025-05-01 13:59:14 -0400

    Basic html calc layout, and operate function.

--- javascript.js
+++ javascript.js
@@ -15,3 +15,28 @@ function multiply(x, y) {
 function divide(x, y) {
     return x / y;
 }
+
+// 3.
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case "+": {
+            add(num1, num2);
+            break;
+        }
+
+        case "-": {
+            subtract(num1, num2);
+            break;
+        }
+
+        case "*": {
+            multiply(num1, num2);
+            break;
+        }
+
+        case "/": {
+            divide(num1, num2);
+        }
+    }
+}
