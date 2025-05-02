28f061345dfd480d6f4c0dc3ad5cea054e5c17e2
caleb <calebtelfe@gmail.com>
2025-05-02 14:14:03 -0400

    button click event listener. Functions to see operator or number pressed.

--- javascript.js
+++ javascript.js
@@ -40,3 +40,50 @@ function operate(operator, num1, num2) {
         }
     }
 }
+
+const buttons = document.querySelectorAll("button");
+const displayScreen = document.querySelector("h2");
+
+
+buttons.forEach(button => {
+    button.addEventListener("click", function() {buttonClick(button);}
+)})
+
+function buttonClick(button) {
+    let buttonPressed = button.textContent;
+
+    let buttonIsNumber = isNumber(buttonPressed);
+    let buttonIsOperator = isOperator(buttonPressed);
+    let screenIsEmpty = isScreenEmpty(displayScreen.textContent);
+
+}
+
+function isNumber(buttonPressed) {
+    if(
+        buttonPressed != "." &&
+        buttonPressed != "=" &&
+        buttonPressed != "+" &&
+        buttonPressed != "-" &&
+        buttonPressed != "x" &&
+        buttonPressed != "/" &&
+        buttonPressed != "DEL") {
+            return true;
+        } else {return false;}
+}
+
+function isOperator(buttonPressed) {} {
+    if(
+        buttonPressed == "." || 
+        buttonPressed == "=" || 
+        buttonPressed == "+" ||
+        buttonPressed == "-" ||
+        buttonPressed == "x" ||
+        buttonPressed == "/" ||
+        buttonPressed == "DEL") {
+            return true;
+        } else {return false;}
+}
+
+function isScreenEmpty(screenText) {
+
+}
